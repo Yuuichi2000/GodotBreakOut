d45115e4ba054bf7e53e9a63fc9e15c86fa80864
Yuu1 <lz9511230610@gmail.com>
2026-07-07 23:01:40 +0900

    继续编写生成砖块逻辑

--- Breakout/assets/Scripts/Block/BlockGenerator.gd
+++ Breakout/assets/Scripts/Block/BlockGenerator.gd
@@ -9,9 +9,23 @@ class_name BlockGenerator
 @export_category("参数")
 
 ## 砖块间距
-@export var space_x: float = 0
-@export var space_y: float = 0
+@export var space: Vector2 = Vector2(0, 0)
+
+## 砖块块数
+@export var block_num_rows: int = 0
+@export var block_num_cols: int = 0
+
+## 所有砖块
+var _blocks: Array[Block] = []
 
 ## 创建所有的砖块
 func create_all_blocks() -> void:
 	pass
+	
+## 注册单个砖块
+func register_block(block: Block) -> void:
+	_blocks.append(block)
+	
+## 销毁单个砖块
+func unregister_block(block: Block) -> void:
+	_blocks.erase(block)

e519f2b988c1e14bc5fb8206e8d4e1bbe2eef072
Yuu1 <lz9511230610@gmail.com>
2026-07-08 09:20:52 +0900

    编写创建砖块逻辑

--- Breakout/assets/Scripts/Block/BlockGenerator.gd
+++ Breakout/assets/Scripts/Block/BlockGenerator.gd
@@ -9,19 +9,43 @@ class_name BlockGenerator
 @export_category("参数")
 
 ## 砖块间距
-@export var space: Vector2 = Vector2(0, 0)
+@export var block_space: Vector2 = Vector2(0, 0)
+
+## 砖块中央基准点
+@export var block_center_pos: Vector2 = Vector2(0, 0)
 
 ## 砖块块数
-@export var block_num_rows: int = 0
 @export var block_num_cols: int = 0
+@export var block_num_rows: int = 0
 
 ## 所有砖块
 var _blocks: Array[Block] = []
 
 ## 创建所有的砖块
 func create_all_blocks() -> void:
-	pass
+	## 以offset为基准计算
+	var half_of_block_num: Vector2 = Vector2(0, 0)
 	
+	half_of_block_num.x = block_num_cols / 2
+	half_of_block_num.y = block_num_rows / 2
+	
+	## 开始创建砖块的坐标
+	var block_start_pos: Vector2 = Vector2(0, 0)
+	
+	block_start_pos.x = (half_of_block_num.x - 0.5) * block_space.x
+	block_start_pos.y = (half_of_block_num.y - 0.5) * block_space.y
+	
+	## 实例化砖块
+	for y in block_num_rows:
+		for x in block_num_cols:
+			var block := block_scene.instantiate() as Block
+			
+			block.position = Vector2(
+			block_start_pos.x + x * block_space.x,
+			block_start_pos.y + y * block_space.y)
+			
+			register_block(block)
+			
 ## 注册单个砖块
 func register_block(block: Block) -> void:
 	_blocks.append(block)
